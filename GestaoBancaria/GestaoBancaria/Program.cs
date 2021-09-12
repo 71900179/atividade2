9ee237e51779e77e84824aff3e21003a57f9dd90
Alexandre Xavier <71900179@aluno.faculdadecotemig.br>
2021-09-12 20:47:28 -0300

    primeiro commit

---
+++ GestaoBancaria/GestaoBancaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GestaoBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
