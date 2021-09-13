567574bff25b1edadb8f5c1aa90a0e8c6296f57a
Alexandre Xavier <71900179@aluno.faculdadecotemig.br>
2021-09-12 21:01:15 -0300

    Criando classes de cliente e conta corrente

--- GestaoBancaria/GestaoBancaria/Program.cs
+++ GestaoBancaria/GestaoBancaria/Program.cs
@@ -9,4 +9,40 @@ namespace GestaoBancaria
             Console.WriteLine("Hello World!");
         }
     }
+
+    class ContaCorrente
+    {
+        public Cliente Cliente { get; }
+        public double Valor { get; private set; }
+
+        public ContaCorrente(Cliente cliente)
+        {
+            Cliente = cliente;
+            Valor = 0;
+        }
+
+        public void Depositar(double valor)
+        {
+            Valor += valor;
+        }
+
+        public void Sacar(double valorSaque)
+        {
+            var saquePermitido = Valor - valorSaque >= 0;
+            if (saquePermitido)
+            {
+                Valor -= valorSaque;
+            }
+        }
+
+        public double ConsultarSaldo()
+        {
+            return Valor;
+        }
+    }
+
+    class Cliente
+    {
+        public string Nome { get; set; }
+    }
 }
